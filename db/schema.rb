53ff101982017de072f9fdaacd8b649165eea9d7
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-03-25 22:29:36 +0900

    plannerのモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_131852) do
+ActiveRecord::Schema.define(version: 2021_03_25_132801) do
+
+  create_table "planners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.integer "gender_id", null: false
+    t.date "birth_date", null: false
+    t.integer "prefecture_id", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_planners_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_planners_on_reset_password_token", unique: true
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
