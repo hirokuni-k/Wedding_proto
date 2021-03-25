c3b0ec216357f4e500a2e7b8a2670ef76c2d4e14
Buchikin <jirafanegra124@ezweb.ne.jp>
2021-03-25 22:32:42 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :planners
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'  
+  }
+  devise_for :planners, controllers: {
+    sessions: 'planners/sessions',
+    passwords: 'planners/passwords',
+    registrations: 'planners/registrations'
+  }
 end
