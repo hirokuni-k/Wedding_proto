3a29f3ba4cac3db42d0e7d2a2761551d6c02ad78
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-03-30 01:19:36 +0900

    備忘録

--- app/controllers/planners/registrations_controller.rb
+++ app/controllers/planners/registrations_controller.rb
@@ -52,7 +52,7 @@ class Planners::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
-  #   super(resource)
+  #   super(resource) ここを変更する
   # end
 
   # The path used after sign up for inactive accounts.
