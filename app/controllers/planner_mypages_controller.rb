eab8c0559156a2f95bb04f7a9708df737204daf9
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-04-02 23:10:06 +0900

    planner_mypages controller作成

--- app/controllers/planner_mypages_controller.rb
+++ app/controllers/planner_mypages_controller.rb
@@ -1,4 +1,14 @@
 class PlannerMypagesController < ApplicationController
+
+     require 'happybirthday'
+
      def index
+          @planner = Planner.find(current_planner.id)
+     end
+
+     def show
+          @planner = Planner.find(current_planner.id)
+          birthday = Happybirthday.born_on(current_planner.birth_date)
+          @birthday = birthday.age.years_old
      end
 end
