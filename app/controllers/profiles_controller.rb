ee61bdbabf781d4ab8b94e88ee9934b34c739671
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-04-07 04:06:01 +0900

    profile controller記述

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,9 @@
 class ProfilesController < ApplicationController
      def show
           @planner = Planner.find(current_planner.id)
+          @planner_profile = Profile.where(@planner.profile)
+          if @planner_profile.present?
+               @profile = Profile.find(current_planner.id)
+          end
      end
 end
