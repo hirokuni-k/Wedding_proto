1b743e4cebb1020596f3bb4a149c71e00b16a512
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-04-09 21:57:33 +0900

    profile controller作成

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,31 @@
 class ProfilesController < ApplicationController
+
+     def new
+          @profile = Profile.new
+     end
+
+     def create
+          profile = Profile.create(profile_params)
+          if profile.save
+               redirect_to profile_path(current_planner)
+          else
+               render 'new'
+          end
+     end
+
      def show
           @planner = Planner.find(current_planner.id)
-          @planner_profile = Profile.where(@planner.profile)
+          @planner_profile = Profile.where(planner_id: @planner.id)
           if @planner_profile.present?
-               @profile = Profile.find(current_planner.id)
+               @profile = current_planner.profile
           end
+     end
+
+     private
+
+     def profile_params
+          params.require(:profile).permit(
+               :last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :prefecture_id, :birth_date, :bridal, :specialty, :history, :favorite, :word, :image).merge(
+               planner_id: current_planner.id)
      end
 end
