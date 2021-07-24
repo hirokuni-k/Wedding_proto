d00cf8003cb7746b5d4c3e1353b11f489bcacde9
mitani1993 <fytim55@gmail.com>
2021-07-24 17:50:18 +0900

    プロフィールページ修正

--- app/models/planner.rb
+++ app/models/planner.rb
@@ -3,12 +3,9 @@ class Planner < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :profile
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8}+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
- 
   validates :nickname, presence: true
-  
-  has_one :profile
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,18 +1,21 @@
 class Profile < ApplicationRecord
-     belongs_to :planner
-     has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender
+  belongs_to :prefecture
+  belongs_to :planner
+  has_one_attached :image
 
-     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
-     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
-     validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
-     validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナを使用してください' }
+  validates :gender_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
 
+  # method
 
-     extend ActiveHash::Associations::ActiveRecordExtensions
-     belongs_to :gender
-     belongs_to :prefecture
-
-     validates :gender_id, numericality: { other_than: 1 } 
-     validates :prefecture_id, numericality: { other_than: 1 } 
-
+  def age
+    birthday = self.birth_date.strftime('%Y%m%d')
+    (Date.today.strftime('%Y%m%d').to_i - birthday.to_i) / 10000
+  end
 end
